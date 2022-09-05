c51f1f860e1c7c75203b3de75598ec45db1b7d2a
Fatima Asif <104876521+fatimasif@users.noreply.github.com>
2022-09-05 22:53:49 +0000

    added turtles file

---
+++ turtles/t2loop.py
@@ -0,0 +1,26 @@
+import turtle 
+
+def sample_function():
+  print("This is a function")
+  print("It can be used multiple times")
+
+wn = turtle.Screen()
+
+crush = turtle.Turtle()
+
+# draw a square with a loop
+
+for i in range (4):
+  crush.forward(50)
+  crush.right(90)
+
+# draw a sqaure manually
+  
+crush.forward(50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)

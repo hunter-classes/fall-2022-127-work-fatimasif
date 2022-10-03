3b04826537a8061b20dd73bd9b5955536bf41468
Fatima Asif <104876521+fatimasif@users.noreply.github.com>
2022-10-03 13:32:02 +0000

    tested the homework and added code about lists

---
+++ chapter-10/list3.py
@@ -0,0 +1,4 @@
+# chapter 10 exercise 5
+# Write a Python function named max that takes a parameter containing a nonempty list of integers and returns the maximum value. (Note: there is a builtin function named max but pretend you cannot use it.)
+
+def max
--- chapter-10/lists2.py
+++ chapter-10/lists2.py
@@ -8,6 +8,11 @@ def average(numlist):
 
     return total / len(numlist)
 
+grades = [90,95,100,90]
+print("grades:",grades)
+average=average(grades)
+print("Average:",average)
+
 # chapter 10 exercise 6
 # Write a function sum_of_squares(xs) that computes the sum of the squares of the numbers in the list xs. For example, sum_of_squares([2, 3, 4]) should return 4+9+16 which is 29:
 

172e1f83c9281cd61e6067538dd94ced071e0553
Fatima Asif <104876521+fatimasif@users.noreply.github.com>
2022-10-03 01:49:19 +0000

    completed chapter 10 exercises 3,4,6 for 10/3 HW

---
+++ chapter-10/lists2.py
@@ -0,0 +1,20 @@
+# chapter 10 exercise 4
+# Write a function called average that takes a list of numbers as a parameter and returns the average of the numbers.
+def average(numlist):
+
+    total = 0
+    for num in numlist:
+        total = total + num
+
+    return total / len(numlist)
+
+# chapter 10 exercise 6
+# Write a function sum_of_squares(xs) that computes the sum of the squares of the numbers in the list xs. For example, sum_of_squares([2, 3, 4]) should return 4+9+16 which is 29:
+
+def sum_of_squares(xs):
+  total = 0 
+  for num in numlist:
+    total = num * num + total
+
+  return total
+  

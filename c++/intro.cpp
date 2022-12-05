a8e542cdd551b7446f880a33c89a0be6849504b9
Fatima Asif <104876521+fatimasif@users.noreply.github.com>
2022-12-05 14:37:02 +0000

    c++ classwork

--- c++/intro.cpp
+++ c++/intro.cpp
@@ -19,7 +19,26 @@
 
 int main()  // here to end of line is a comment 
 {
-  std::cout << "Hello World!" << std::endl;
+  int x = 21;
+  double d = 21;
+  char c;
+  std::string s = "this is a string";
+  
+//  int y;
+
+//  y = 30;
+  
+//    int a=10, b=20;
+  
+  std::cout << "Hello World!\n" << std::endl;
+  std::cout << "the var x = " << x << "\n";
+  std::cout << "Doubles: " <<d<< " " <<d/2<< std::endl;
+  // int returns value 10 instead of 10.5 because it deals with integers
+  std::cout << "Ints: " <<x<< " " <<x/2<< std::endl;
+
+  d = x/2.0;
+  std::cout << d << "\n";
   
   return 0;
+  
 }

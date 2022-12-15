1e752eb6c1a502ce120bb14eb0916c227c36b406
Fatima Asif <104876521+fatimasif@users.noreply.github.com>
2022-12-15 02:26:35 +0000

    completed base c++ project and extras

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,25 +1,36 @@
 // Project-04-C++
-// Base Project: Write a working C++ program in a file named main.cpp in your cpp folder. The program should print out the string “Hello World!” followed by a newline.
 
 #include <iostream>
+#include <cstdlib> 
+
+// Extra 2: Create a working function with a parameter
+// Make sure you’ve implemeted at least one function that takes in at least one parameter and returns a value. For example. if you determine the abolute value of a number for your program, you could write a function that takes in an integer parameter and returns its absolue value.
+int absoluteValue(int num) {
+  // If the number is a negative, it will return the positive value of the number
+  if (num < 0) {
+    return -1 * num;
+  }
+  // If the number is positive then it will return the number itself
+  else {
+    return num;
+  }
+}
 
 int main()
 {
+// Base Project: Write a working C++ program in a file named main.cpp in your cpp folder. The program should print out the string “Hello World!” followed by a newline.
 std::cout << "Hello World!" << std::endl;
-return 0;
-
 
 // Extra 1
-// your program must show that you understand an if statement and a loop. You can modify your program in any way to exhibit this.
-
-// Determine which of two numbers is greater
-// Determine the absolute value of a number
-// Calculate the sum of values from 1 to some larger numbers
+// Your program must show that you understand an if statement and a loop. You can modify your program in any way to exhibit this.
 
 {
 // Determine which of two numbers is greater
-int num1 = 10;
-int num2 = 20;
+// Choosing a random number from the range 0 - 99 (std::rand() % 100)
+int num1 = std::rand() % 100;
+int num2 = std::rand() % 100;
+
+std::cout << "The two numbers are: " << num1 << " and " << num2 << std::endl;
 
 if (num1 > num2)
 {
@@ -38,45 +49,29 @@ num3 = -num3;
 }
 std::cout << "The absolute value of " << num3 << " is " << num3 << std::endl;
 
-// Calculate the sum of values from 1 to some larger number
-int num4 = 100;
+// Calculate the sum of values from 1 to a random number (0 - 99)
+int num4 = std::rand() % 100;
 int sum = 0;
 for (int i = 1; i <= num4; i++)
 {
 sum += i;
 }
 std::cout << "The sum of values from 1 to " << num4 << " is " << sum << std::endl;
+}
 
-return 0;
+// Creating a for loop that prints the numbers 1 - 10
+for(int i = 1; i <= 10; i++){
+  std::cout << i << std::endl;
 }
 
 // Extra 2
-// Make sure you’ve implemeted at least one function that takes in at least one parameter and returns a value. For example. if you determine the abolute value of a number for your program, you could write a function that takes in an integer parameter and returns its absolue value.
+// Testing the function 
+  int getAbsoluteValue(int x);
+  // print result(s) of diff trials
+  std::cout << absoluteValue(100) << std::endl;
+  std::cout << absoluteValue(-22) << std::endl;
+  std::cout << absoluteValue(-7) << std::endl;
 
-// Function that takes in an integer parameter and returns its absolute value
-int getAbsoluteValue(int num)
-{
-if (num < 0)
-{
-num = -num;
-}
-return num;
-}
-
-{
-// Determine the absolute value of a number
-int num = -10;
-int abs_value = getAbsoluteValue(num);
-std::cout << "The absolute value of " << num << " is " << abs_value << std::endl;
-
-return 0;
-}
+  return 0;
 
 }
-
-
-
-
-
-
-
